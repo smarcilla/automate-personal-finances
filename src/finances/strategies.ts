cf21337febbc10c62924341a8ff7c17a07eb61b4
Sergio Marcilla <sergio@devgurus.io>
2023-01-12 19:33:56 +0100

    Added PersonalFinancesStrategy class implementing FinancesStrategy interface for personal finances management in Finances library

--- src/finances/strategies.ts
+++ src/finances/strategies.ts
@@ -1,3 +1,12 @@
-class StrategyFacade {
-    
+import { FinancesStrategy } from './interfaces';
+
+export class PersonalFinancesStrategy implements FinancesStrategy {
+  execute(): void {
+    //TODO: Step 1 -> Uses datasource with finance data.
+
+    //TODO: Step 2 -> Calcule incomes/expenses representing personal finalces
+
+    //TODO: Step 3 -> Generate output format of report & send it user device
+    throw new Error('Method not implemented.');
+  }
 }
